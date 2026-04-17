264228cd335d00c6e38b4f5df155cea1bcc07377
Luís Miguel <vilanovamiguel1@gmail.com>
2026-04-17 13:01:49 -0300

    feat: adicionado o model pessoa e registrado no site admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    curso = models.CharField(max_length=100)
+    periodo = models.CharField(max_length=20)
+    email = models.TextField()
+    link_git = models.URLField()
+    link_linkedin = models.URLField()
+    url= models.URLField()
+
+    def __str__(self):
+        return self.nome
 # Create your models here.
